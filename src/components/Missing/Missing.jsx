698f441e6d0eb7552013bac08eea37f6fc69059a
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-03-25 17:07:17 +0000

    loading

---
+++ src/components/Missing/Missing.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import './Missing.css';
+
+const Missing = () => {
+    return (
+        <main className="Missing">
+            <h2>Page Not Found</h2>
+            <p>Well, That is disappointing.</p>
+            <p>
+                <Link to="/"> Visit Our Homepage</Link>
+            </p>
+        </main>
+    );
+};
+
+export default Missing;
