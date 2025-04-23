d729a7bbb390d835144b61e5e0a0e51dfebe1459
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-04-23 23:44:34 +0200

    search

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,42 @@
+
+
+import './Search.css'
+
+const Search = ({setMovies, loading, setLoading, setError, search, setSearch, searchFun}) => {
+    
+
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        if(!search.trim()) return;
+        if(loading) return;
+        setLoading(true);
+        try {
+            const searchResult = await searchFun(search);
+            setMovies(searchResult);
+            setError(null);
+        } catch (err) {
+          console.log(err.message)
+          setError(err.message);
+
+        }finally{
+          setLoading(false);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSearch} className="search-form">
+            <input
+                type="text"
+                className="search-input"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Search for movies..."
+            />
+            <button type="submit" className="search-button">
+                Search
+            </button>
+        </form>
+    )
+}
+
+export default Search
