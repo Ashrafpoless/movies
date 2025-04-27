2846cd26bfea67bbdc3a9d5badd3161f170870c9
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-04-27 22:42:05 +0200

    fixHome

--- src/pages/Person/Person.jsx
+++ src/pages/Person/Person.jsx
@@ -3,6 +3,7 @@ import { getPopularMovies, searchPerson } from '../../Api/moviesApi';
 
 import MovieCard from '../../components/MovieCard/MovieCard';
 import Loading from '../../components/Loading/Loading';
+import Search from '../../components/Search/Search';
 
 import './Person.css';
 
@@ -16,7 +17,6 @@ const Person = () => {
     const [numberOfPages, setNumberOfPages] = useState(null)
     
 
-    //const movies = getPopularMovies();
 
     useEffect(() => {
         const loadPopularMovies = async () => {
@@ -35,23 +35,7 @@ const Person = () => {
         loadPopularMovies();
     }, [page]);
 
-    const handleSearch = async (e) => {
-        e.preventDefault();
-        if(!search.trim()) return;
-        if(loading) return;
-        setLoading(true);
-        try {
-            const searchResult = await searchPerson(search);
-            setMovies(searchResult);
-            setError(null);
-        } catch (err) {
-          console.log(err.message)
-          setError(err.message);
-
-        }finally{
-          setLoading(false);
-        }
-    };
+    
 
    // **************** Pagination ****************
     
@@ -76,18 +60,9 @@ const nextPage = () => {
 
     return (
         <div className="home">
-           <form onSubmit={handleSearch} className="search-form">
-                        <input
-                            type="text"
-                            className="search-input"
-                            value={search}
-                            onChange={(e) => setSearch(e.target.value)}
-                            placeholder="Search for movies..."
-                        />
-                        <button type="submit" className="search-button">
-                            Search
-                        </button>
-            </form>
+          
+          <Search setMovies={setMovies} loading={loading} setLoading={setLoading} setError={setError} search={search} setSearch={setSearch} searchFun={searchPerson}/>
+
             {error && <div  className="error-message">Error: {error}</div>}
             {loading ? (
                 <Loading/>
